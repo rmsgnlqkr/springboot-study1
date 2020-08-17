3d004d59a065c219b4d29cb089e815e80d49a3bc
rmsgnlqkr <rmsgnlqkr@naver.com>
2020-08-17 20:34:15 +0900

    ver1.1

---
+++ src/main/java/com/springboot/example/study/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.springboot.example.study.web;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.example.study.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+}
