495655b9fda07af668675d18d20a1ac12920e13e
rmsgnlqkr <rmsgnlqkr@naver.com>
2020-09-03 00:36:30 +0900

    ver 1.1.3

--- src/main/java/com/springboot/example/study/web/IndexController.java
+++ src/main/java/com/springboot/example/study/web/IndexController.java
@@ -2,6 +2,7 @@ package com.springboot.example.study.web;
 
 import javax.servlet.http.HttpSession;
 
+import com.springboot.example.study.config.auth.LoginUser;
 import com.springboot.example.study.config.auth.dto.SessionUser;
 import com.springboot.example.study.service.posts.PostsService;
 import com.springboot.example.study.web.dto.PostsResponseDto;
@@ -18,13 +19,14 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
 
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser)httpSession.getAttribute("user");
-        if(user != null){
+        if (user != null) {
             model.addAttribute("userName", user.getName());
+            model.addAttribute("userEmail", user.getEmail());
+            model.addAttribute("userPicture", user.getPicture());
         }
 
         return "index";
