f25f1dae987699370bae5883f8556a30551dbf64
rmsgnlqkr <rmsgnlqkr@naver.com>
2020-09-02 22:37:43 +0900

    ver 1.0.9

---
+++ src/main/java/com/springboot/example/study/web/IndexController.java
@@ -0,0 +1,40 @@
+package com.springboot.example.study.web;
+
+import javax.servlet.http.HttpSession;
+
+import com.springboot.example.study.service.posts.PostsService;
+import com.springboot.example.study.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model) {
+
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+}
